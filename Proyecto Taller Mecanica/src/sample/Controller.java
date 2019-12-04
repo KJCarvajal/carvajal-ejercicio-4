1538b078237558b682eecc74f766d5e925926074
KJCarvajal <kevincarbajal030@gmail.com>
2019-12-04 09:34:56 -0600

    Arregle el disenio de los botones

---
+++ Proyecto Taller Mecanica/src/sample/Controller.java
@@ -0,0 +1,54 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    //region Variables de traidas de Scene Builder
+    @FXML private VBox VBoxMenuLateralLeft;
+    @FXML private ImageView btnImgMecanikMenuInicio;
+    @FXML private Button btnMenuInicio;
+
+    //endregion
+
+    //region Variable Locales
+    private boolean menuOcultado = false;
+
+    //endregion
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+    }
+
+    public void onOcultarMenuClicked(MouseEvent event){
+
+        if(btnMenuInicio.getText().equals("")){
+            VBoxMenuLateralLeft.setPrefWidth(180);
+            btnMenuInicio.setText("MecaniK");
+            btnImgMecanikMenuInicio.setFitWidth(140);
+            btnImgMecanikMenuInicio.setFitHeight(135);
+
+        } else {
+            VBoxMenuLateralLeft.setPrefWidth(45);
+            btnMenuInicio.setText("");
+            btnImgMecanikMenuInicio.setFitWidth(40);
+            btnImgMecanikMenuInicio.setFitHeight(40);
+        }
+    }
+
+    public void onExitButtonClicked(MouseEvent event) {
+        Platform.exit();
+        System.exit(0);
+    }
+
+}
+
---
+++ Proyecto Taller Mecanica/src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("frmPrincipal.fxml"));
+
+        //primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setTitle("MecaniK");
+        Scene scene = new Scene(root);
+        //scene.setFill(Color.RED);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        //#0dbf3d <-- Color verde primario
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
