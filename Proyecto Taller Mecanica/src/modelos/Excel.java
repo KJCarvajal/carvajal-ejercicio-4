c544ed254f71ea268b52ab5ebd0003e97f2c4630
LordRevan0 <49046105+LordRevan0@users.noreply.github.com>
2019-12-12 04:05:33 -0600

    Commits Miltonianos
    ControllerCorreo
    Correo
    Excel

---
+++ Proyecto Taller Mecanica/src/modelos/Excel.java
@@ -0,0 +1,31 @@
+package com.Revan;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Excel {
+
+    public static void crearExcel() {
+        Workbook book = new XSSFWorkbook();
+        Sheet sheet = book.createSheet("Presupuesto");
+        Row row = sheet.createRow(0);
+        row.createCell(0).setCellValue("Hola Mundo");
+
+        try {
+            FileOutputStream fileout = new FileOutputStream("presupuesto.xlsx");
+            book.write(fileout);
+            fileout.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
